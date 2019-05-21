5bf82b5a44fdacbf470a35f9212743ea35e0c275
amateos-stratio <amateos@stratio.com>
2019-05-21 15:19:24 +0200

    [AT][QATM-2303] Create user,policy and groups if they don exist previously

--- src/main/java/com/stratio/qa/specs/RestSpec.java
+++ src/main/java/com/stratio/qa/specs/RestSpec.java
@@ -226,6 +226,202 @@ public class RestSpec extends BaseGSpec {
         commonspec.setResponse(requestType, response.get());
     }
 
+
+    /**
+     *Creates a custom user in gosec management if the userId doesn't exist
+     *
+     * @param userId
+     * @param endPoint
+     * @param foo
+     * @param loginInfo
+     * @param baseData
+     * @param baz
+     * @param type
+     * @param modifications
+     * @throws Exception
+     */
+    @When("^I create user '(.+?)' to '(.+?)'( with user and password '(.+:.+?)')? if it does not exist based on '([^:]+?)'( as '(json|string|gov)')? with:$")
+    public void createUserIfNotExist(String userId, String endPoint, String foo, String loginInfo, String baseData, String baz, String type, DataTable modifications) throws Exception {
+        // Retrieve data
+        String retrievedData = commonspec.retrieveData(baseData, type);
+
+        // Modify data
+        commonspec.getLogger().debug("Modifying data {} as {}", retrievedData, type);
+        String modifiedData = commonspec.modifyData(retrievedData, type, modifications).toString();
+
+        Integer expectedStatusCreate = new Integer(201);
+        String endPointUser = endPoint + "/" + userId;
+        String user = null;
+        String password = null;
+
+        if (loginInfo != null) {
+            user = loginInfo.substring(0, loginInfo.indexOf(':'));
+            password = loginInfo.substring(loginInfo.indexOf(':') + 1, loginInfo.length());
+        }
+
+        //Get user
+        commonspec.getLogger().debug("Generating request {} to {} with data {} as {}", new Object[]{"GET", endPointUser, null, null});
+        Future<Response> response = commonspec.generateRequest("GET", false, user, password, endPointUser, null, null, "");
+        commonspec.getLogger().debug("Saving response");
+        commonspec.setResponse("GET", (Response) response.get());
+
+        if (commonspec.getResponse().getStatusCode() != 200) {
+            //Create user if doesn't exist
+            commonspec.getLogger().debug("Generating request {} to {} with data {} as {}", new Object[]{"POST", endPoint, modifiedData, type});
+            Future<Response> responseCreateUser = commonspec.generateRequest("POST", false, user, password, endPoint, modifiedData, type, "");
+            commonspec.getLogger().debug("Saving response");
+            commonspec.setResponse("POST", (Response) responseCreateUser.get());
+
+            try {
+                if (commonspec.getResponse().getStatusCode() == 409) {
+                    commonspec.getLogger().warn("User {} already exists", userId);
+                } else {
+                    assertThat(commonspec.getResponse().getStatusCode()).isEqualTo(expectedStatusCreate);
+                    commonspec.getLogger().warn("User {} created", userId);
+                }
+            } catch (Exception e) {
+                commonspec.getLogger().warn("Error creating user {}: {}", userId, commonspec.getResponse().getResponse());
+                throw e;
+            }
+
+        }
+    }
+
+
+    /**
+     *Creates a custom group in gosec management if the userId doesn't exist
+     *
+     * @param groupId
+     * @param endPoint
+     * @param foo
+     * @param loginInfo
+     * @param baseData
+     * @param baz
+     * @param type
+     * @param modifications
+     * @throws Exception
+     */
+    @When("^I create group '(.+?)' to '(.+?)'( with user and password '(.+:.+?)')? if it does not exist based on '([^:]+?)'( as '(json|string|gov)')? with:$")
+    public void createGroupIfNotExist(String groupId, String endPoint, String foo, String loginInfo, String baseData, String baz, String type, DataTable modifications) throws Exception {
+        // Retrieve data
+        String retrievedData = commonspec.retrieveData(baseData, type);
+
+        // Modify data
+        commonspec.getLogger().debug("Modifying data {} as {}", retrievedData, type);
+        String modifiedData = commonspec.modifyData(retrievedData, type, modifications).toString();
+
+        Integer expectedStatusCreate = new Integer(201);
+        String endPointGroup = endPoint + "/" + groupId;
+        String user = null;
+        String password = null;
+
+        if (loginInfo != null) {
+            user = loginInfo.substring(0, loginInfo.indexOf(':'));
+            password = loginInfo.substring(loginInfo.indexOf(':') + 1, loginInfo.length());
+        }
+
+        //Get Group
+        commonspec.getLogger().debug("Generating request {} to {} with data {} as {}", new Object[]{"GET", endPointGroup, null, null});
+        Future<Response> response = commonspec.generateRequest("GET", false, user, password, endPointGroup, null, null, "");
+        commonspec.getLogger().debug("Saving response");
+        commonspec.setResponse("GET", (Response) response.get());
+
+        if (commonspec.getResponse().getStatusCode() != 200) {
+            //Create group if doesn't exist
+            commonspec.getLogger().debug("Generating request {} to {} with data {} as {}", new Object[]{"POST", endPoint, modifiedData, type});
+            Future<Response> responseCreateGroup = commonspec.generateRequest("POST", false, user, password, endPoint, modifiedData, type, "");
+            commonspec.getLogger().debug("Saving response");
+            commonspec.setResponse("POST", (Response) responseCreateGroup.get());
+
+            try {
+                if (commonspec.getResponse().getStatusCode() == 409) {
+                    commonspec.getLogger().warn("Group {} already exists", groupId);
+                } else {
+                    assertThat(commonspec.getResponse().getStatusCode()).isEqualTo(expectedStatusCreate);
+                    commonspec.getLogger().warn("Group {} created", groupId);
+                }
+            } catch (Exception e) {
+                commonspec.getLogger().warn("Error creating Group {}: {}", groupId, commonspec.getResponse().getResponse());
+                throw e;
+            }
+
+        }
+    }
+
+    /**
+     *Creates a policy in gosec management if the policyId doesn't exist. If it exists, the old one is deleted before the creation.
+     *
+     * @param policyId
+     * @param endPoint
+     * @param foo
+     * @param loginInfo
+     * @param baseData
+     * @param baz
+     * @param type
+     * @param modifications
+     * @throws Exception
+     */
+    @When("^I create policy '(.+?)' to '(.+?)'( with user and password '(.+:.+?)')? if it does not exist based on '([^:]+?)'( as '(json|string|gov)')? with:$")
+    public void createPolicyIfNotExist(String policyId, String endPoint, String foo, String loginInfo, String baseData, String baz, String type, DataTable modifications) throws Exception {
+        // Retrieve data
+        String retrievedData = commonspec.retrieveData(baseData, type);
+
+        // Modify data
+        commonspec.getLogger().debug("Modifying data {} as {}", retrievedData, type);
+        String modifiedData = commonspec.modifyData(retrievedData, type, modifications).toString();
+
+        Integer expectedStatusCreate = new Integer(201);
+        Integer expectedStatusDelete = new Integer(200);
+        String endPointPolicy = endPoint + "/" + policyId;
+        String user = null;
+        String password = null;
+
+        if (loginInfo != null) {
+            user = loginInfo.substring(0, loginInfo.indexOf(':'));
+            password = loginInfo.substring(loginInfo.indexOf(':') + 1, loginInfo.length());
+        }
+
+        //Get Policy
+        commonspec.getLogger().debug("Generating request {} to {} with data {} as {}", new Object[]{"GET", endPointPolicy, null, null});
+        Future<Response> response = commonspec.generateRequest("GET", false, user, password, endPointPolicy, null, null, "");
+        commonspec.getLogger().debug("Saving response");
+        commonspec.setResponse("GET", (Response) response.get());
+
+        if (commonspec.getResponse().getStatusCode() == 200) {
+            //Delete policy if exists
+            commonspec.getLogger().debug("Generating request {} to {} with data {} as {}", new Object[]{"DELETE", endPointPolicy, null, null});
+            Future<Response> responseDeletePolicy = commonspec.generateRequest("DELETE", false, user, password, endPointPolicy, null, null, "");
+            commonspec.getLogger().debug("Saving response");
+            commonspec.setResponse("DELETE", (Response) responseDeletePolicy.get());
+
+            try {
+                assertThat(commonspec.getResponse().getStatusCode()).isEqualTo(expectedStatusDelete);
+            } catch (Exception e) {
+                commonspec.getLogger().warn("Error deleting Policy {}: {}", policyId, commonspec.getResponse().getResponse());
+                throw e;
+            }
+        }
+
+        //Create policy
+        commonspec.getLogger().debug("Generating request {} to {} with data {} as {}", new Object[]{"POST", endPoint, modifiedData, type});
+        Future<Response> responseCreatePolicy = commonspec.generateRequest("POST", false, user, password, endPoint, modifiedData, type, "");
+        commonspec.getLogger().debug("Saving response");
+        commonspec.setResponse("POST", (Response) responseCreatePolicy.get());
+
+        try {
+            if (commonspec.getResponse().getStatusCode() == 409) {
+                commonspec.getLogger().warn("Policy {} already exists", policyId);
+            } else {
+                assertThat(commonspec.getResponse().getStatusCode()).isEqualTo(expectedStatusCreate);
+                commonspec.getLogger().warn("Policy {} created", policyId);
+            }
+        } catch (Exception e) {
+            commonspec.getLogger().warn("Error creating Policy {}: {}", policyId, commonspec.getResponse().getResponse());
+            throw e;
+        }
+
+    }
+
     /**
      * Same sendRequest, but in this case, we do not receive a data table with modifications.
      * Besides, the data and request header are optional as well.
@@ -240,7 +436,8 @@ public class RestSpec extends BaseGSpec {
      * @throws Exception
      */
     @When("^I send a '(.+?)' request to '(.+?)'( with user and password '(.+:.+?)')?( based on '([^:]+?)')?( as '(json|string|gov)')?$")
-    public void sendRequestNoDataTable(String requestType, String endPoint, String foo, String loginInfo, String bar, String baseData, String baz, String type) throws Exception {
+    public void sendRequestNoDataTable(String requestType, String endPoint, String foo, String loginInfo, String
+            bar, String baseData, String baz, String type) throws Exception {
         Future<Response> response;
         String user = null;
         String password = null;
@@ -276,7 +473,8 @@ public class RestSpec extends BaseGSpec {
      * @throws Exception
      */
     @When("^in less than '(\\d+?)' seconds, checking each '(\\d+?)' seconds, I send a '(.+?)' request to '(.+?)'( so that the response( does not)? contains '(.+?)')?$")
-    public void sendRequestTimeout(Integer timeout, Integer wait, String requestType, String endPoint, String foo, String contains, String responseVal) throws Exception {
+    public void sendRequestTimeout(Integer timeout, Integer wait, String requestType, String endPoint, String
+            foo, String contains, String responseVal) throws Exception {
 
         AssertionError ex = null;
         String type = "";
@@ -363,18 +561,21 @@ public class RestSpec extends BaseGSpec {
     }
 
     @Then("^the service response must contain the text '(.*?)'$")
-    public void assertResponseMessage(String expectedText) throws ClassNotFoundException, NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+    public void assertResponseMessage(String expectedText) throws
+            ClassNotFoundException, NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
         Pattern pattern = CommonG.matchesOrContains(expectedText);
         assertThat(commonspec.getResponse().getResponse()).containsPattern(pattern);
     }
 
     @Then("^the service response must not contain the text '(.*?)'$")
-    public void assertNegativeResponseMessage(String expectedText) throws ClassNotFoundException, NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+    public void assertNegativeResponseMessage(String expectedText) throws
+            ClassNotFoundException, NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
         assertThat(commonspec.getResponse().getResponse()).doesNotContain(expectedText);
     }
 
     @Then("^the service response status must be '(.*?)'( and its response length must be '(.*?)' | and its response must contain the text '(.*?)')?$")
-    public void assertResponseStatusLength(Integer expectedStatus, String foo, Integer expectedLength, String expectedText) {
+    public void assertResponseStatusLength(Integer expectedStatus, String foo, Integer expectedLength, String
+            expectedText) {
         if (foo != null) {
             if (foo.contains("length")) {
                 assertThat(Optional.of(commonspec.getResponse())).hasValueSatisfying(r -> {
